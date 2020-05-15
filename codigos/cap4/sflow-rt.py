5cbdf32162cfc30ac1aa304df29a3b349194d56c
Ramon Fontes <ramonreisfontes@gmail.com>
2020-05-15 09:28:52 -0300

    update sflow-rt

--- codigos/cap4/sflow-rt.py
+++ codigos/cap4/sflow-rt.py
@@ -19,15 +19,14 @@ import re
 
 def topology():
     "Create a network."
-    net = Mininet_wifi(controller=Controller, link=wmediumd,
-                       wmediumd_mode=interference)
+    net = Mininet_wifi(link=wmediumd, wmediumd_mode=interference)
 
     info("*** Creating nodes\n")
     sta1 = net.addStation('sta1', mac='00:00:00:00:00:11', position='1,1,0')
     sta2 = net.addStation('sta2', mac='00:00:00:00:00:12', position='31,11,0')
-    ap1 = net.addAccessPoint('ap1', wlans=2, ssid='ssid1,', position='10,10,0')
-    ap2 = net.addAccessPoint('ap2', wlans=2, ssid='ssid2,', position='30,10,0')
-    ap3 = net.addAccessPoint('ap3', wlans=2, ssid='ssid3,', position='50,10,0')
+    ap1 = net.addAccessPoint('ap1', wlans=2, ssid=['ssid1','mesh'], position='10,10,0')
+    ap2 = net.addAccessPoint('ap2', wlans=2, ssid=['ssid2','mesh'], position='30,10,0')
+    ap3 = net.addAccessPoint('ap3', wlans=2, ssid=['ssid3','mesh'], position='50,10,0')
     c0 = net.addController('c0')
 
     info("*** Configuring wifi nodes\n")
@@ -40,21 +39,21 @@ def topology():
     net.addLink(ap2, intf='ap2-wlan2', cls=mesh, ssid='mesh-ssid', channel=5)
     net.addLink(ap3, intf='ap3-wlan2', cls=mesh, ssid='mesh-ssid', channel=5)
 
-    info("*** Starting network\n")
+    info("*** Building network\n")
     net.build()
     c0.start()
     ap1.start([c0])
     ap2.start([c0])
     ap3.start([c0])
 
-    ap1.cmd('iw dev %s-mp1 interface add %s-mon0 type monitor' %
+    ap1.cmd('iw dev %s-mp2 interface add %s-mon0 type monitor' %
                  (ap1.name, ap1.name))
-    ap2.cmd('iw dev %s-mp1 interface add %s-mon0 type monitor' %
+    ap2.cmd('iw dev %s-mp2 interface add %s-mon0 type monitor' %
                  (ap2.name, ap2.name))
     ap1.cmd('ifconfig %s-mon0 up' % ap1.name)
     ap2.cmd('ifconfig %s-mon0 up' % ap2.name)
 
-    ifname='enp2s0'
+    ifname='enp2s0'  # have to be changed to your own iface!
     collector = environ.get('COLLECTOR','127.0.0.1')
     sampling = environ.get('SAMPLING','10')
     polling = environ.get('POLLING','10')
@@ -63,7 +62,7 @@ def topology():
 
     for ap in net.aps:
         sflow += ' -- set bridge %s sflow=@sflow' % ap
-        print ' '.join([ap.name for ap in net.aps])
+        info(' '.join([ap.name for ap in net.aps]))
         quietRun(sflow)
 
     agent = '127.0.0.1'
@@ -83,31 +82,20 @@ def topology():
     path = '/sys/devices/virtual/net/'
     for child in listdir(path):
         parts = re.match('(^.+)-(.+)', child)
-        if parts == None: continue
+        if parts is None: continue
         if parts.group(1) in topo['nodes']:
             ifindex = open(path+child+'/ifindex').read().split('\n',1)[0]
             topo['nodes'][parts.group(1)]['ports'][child] = {'ifindex': ifindex}
 
-    i = 0
-    for ap1 in net.aps:
-        j = 0
-        for ap2 in net.aps:
-            if j > i:
-                linkName = '%s-%s' % (ap1.name, ap2.name)
-                topo['links'][linkName] = {'node1': ap1.name, 'port1': 'ap1-mp1',
-                                           'node2': ap2.name, 'port2': 'ap2-mp1'}
-                #topo['links'][linkName] = {'node1': ap1.name, 'port1': 'ap1-mon0',
-                                          # 'node2': ap2.name, 'port2': 'ap2-mon0'}
-
-                intfs = ap1.connectionsTo(ap2)
-                for intf in intfs:
-                    #ap1ifIdx = topo['nodes'][ap1.name]['ports'][intf[0].name]['ifindex']
-                    #ap2ifIdx = topo['nodes'][ap2.name]['ports'][intf[1].name]['ifindex']
-                    linkName = '%s-%s' % (ap1.name, ap2.name)
-                    topo['links'][linkName] = {'node1': ap1.name, 'port1': intf[0].name,
-                                               'node2': ap2.name, 'port2': intf[1].name}
-            j += 1
-        i += 1
+    linkName = '%s-%s' % (ap1.name, ap2.name)
+    topo['links'][linkName] = {'node1': ap1.name, 'port1': 'ap1-mp2',
+                               'node2': ap2.name, 'port2': 'ap2-mp2'}
+    linkName = '%s-%s' % (ap2.name, ap3.name)
+    topo['links'][linkName] = {'node1': ap2.name, 'port1': 'ap2-mp2',
+                               'node2': ap3.name, 'port2': 'ap3-mp2'}
+    linkName = '%s-%s' % (ap1.name, ap2.name)
+    topo['links'][linkName] = {'node1': ap1.name, 'port1': ap1.wintfs[0].name,
+                               'node2': ap2.name, 'port2': ap2.wintfs[0].name}
 
     put('http://127.0.0.1:8008/topology/json',data=dumps(topo))
 
